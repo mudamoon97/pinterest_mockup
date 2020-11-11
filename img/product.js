5ebda1785f5fe908de9864f0c02c9cb96c682e87
mudamoon97 <53881669+mudamoon97@users.noreply.github.com>
2020-11-11 11:36:30 +0100

    pinterest mockup

---
+++ img/product.js
@@ -0,0 +1,43 @@
+export const product = {
+    colOne: [
+      "../images/1.jpg",
+      "../images/2.jpg",
+      "../images/3.jpg",
+      "../images/4.jpg",
+    ],
+    colTwo: [
+      "../images/5.jpg",
+      "../images/6.jpg",
+      "../images/7.jpg",
+      "../images/8.jpg",
+      "../images/14.jpg",
+    ],
+    colThree: [
+      "../images/10.jpg",
+      "../images/11.jpg",
+      "../images/12.jpg",
+      "../images/13.jpg",
+    ],
+    colFour: [
+      "../images/14.jpg",
+      "../images/15.jpg",
+      "../images/2.jpg",
+      "../images/3.jpg",
+      "../images/16.jpg",
+    ],
+    colFive: [
+      "../images/9.jpg",
+      "../images/3.jpg",
+      "../images/6.jpg",
+      "../images/11.jpg",
+    ],
+  };
+  export const searchPopular = [
+    { title: "Blue aesthetic wallpaper", url: "../images/pin1.png" },
+    { title: "Creative wallpaper", url: "../images/pin3.png" },
+    { title: "Phone backgrounds", url: "../images/pin2.png" },
+    { title: "Living room designs", url: "../images/pin4.png" },
+    { title: "Wave wallpaper", url: "../images/pin2.png" },
+    { title: "Neon signs", url: "../images/pin3.png" },
+  ];
+  
